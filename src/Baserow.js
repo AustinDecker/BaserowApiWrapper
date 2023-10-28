4da1704e7233138e929c1ba5944b6dccc632d3a2
Austin Decker <deckaj01@pfw.edu>
2023-10-28 15:37:25 -0400

    created basic Baserow API

---
+++ src/Baserow.js
@@ -0,0 +1,109 @@
+import axios from "axios";
+
+export default function Baserow(api_token){
+    const baserowInstance = {
+        api_token,
+        
+       getTable,
+       getRow,
+       createRow,
+       updateRow,
+       deleteRow
+
+    }
+    return baserowInstance;
+
+    //private helper functions
+    function getTable(tableID, {search = "", size=100, page=1}){
+        return async function(){
+            try {
+                let response = await axios({
+                    url: `https://api.baserow.io/api/database/rows/table/${tableID}/?user_field_names=true&search=${search}&size=${size}&page=${page}`,
+                    method: "get",
+                    headers: {
+                        "Authorization": api_token
+                    }
+                })
+                return response.data;
+            } catch (error) {
+                console.log(error.message);
+            }
+            
+        }
+    }
+
+    function getRow(tableID, rowID){
+        return async function(){
+            try {
+                let response = await axios({
+                    url: `https://api.baserow.io/api/database/rows/table/${tableID}/${rowID}/?user_field_names=true`,
+                    method: "get",
+                    headers: {
+                        "Authorization": api_token
+                    }
+                })
+                return response.data;
+            } catch (error) {
+                console.log(error.message);
+            }
+            
+        }
+    }
+
+    function createRow(tableID, row_fields){
+        return async function(){
+            try {
+                let response = await axios({
+                    url: `https://api.baserow.io/api/database/rows/table/${tableID}/?user_field_names=true`,
+                    method: "post",
+                    headers: {
+                        "Authorization": api_token,
+                        "Content-Type": "application/json"
+                    },
+                    data: JSON.stringify(row_fields)
+                })
+                return response.data;
+            } catch (error) {
+                console.log(error.message);
+            }
+        }
+    }
+
+    function updateRow(tableID, rowID, row_fields){
+        return async function(){
+            try {
+                let response = await axios({
+                    url: `https://api.baserow.io/api/database/rows/table/${tableID}/${rowID}?user_field_names=true`,
+                    method: "patch",
+                    headers: {
+                        "Authorization": api_token,
+                        "Content-Type": "application/json"
+                    },
+                    data: JSON.stringify(row_fields)
+                })
+                return response.data;
+            } catch (error) {
+                console.log(error.message);
+            }
+        }
+    }
+
+    function deleteRow(tableID, rowID){
+        return async function(){
+            try {
+                let response = await axios({
+                    url: `https://api.baserow.io/api/database/rows/table/${tableID}/${rowID}?user_field_names=true`,
+                    method: "delete",
+                    headers: {
+                        "Authorization": api_token,
+                        "Content-Type": "application/json"
+                    },
+                    data: JSON.stringify(row_fields)
+                })
+                return response.data;
+            } catch (error) {
+                console.log(error.message);
+            }
+        }
+    }
+}
---
+++ src/SMCBaserow.js
@@ -0,0 +1,55 @@
+import Baserow from "./Baserow";
+
+const API_KEY = "dummy key";
+
+const SMCBaserow = Baserow(API_KEY);
+
+SMCBaserow.getSMCPeopleTable = async function(options){
+    return await this.getTable(212079, options)();
+}
+
+SMCBaserow.getSMCPerson = async function(rowID){
+    return await this.getRow(212079, rowID)();
+}
+
+SMCBaserow.getRoomsTable = async function(options){
+    return await this.getTable(212080, options)();
+}
+
+SMCBaserow.getRoom = async function(rowID){
+    return await this.getRow(212080, rowID)();
+}
+
+SMCBaserow.getEventsTable = async function(options){
+    return await getTable(212081, options)();
+}
+
+SMCBaserow.getEvent = async function(rowID){
+    return await getRow(212081, rowID)();
+}
+
+SMCBaserow.getGearsTable = async function(options){
+    return await getTable(212082, options)();
+}
+
+SMCBaserow.getGear = async function(rowID){
+    return await getRow(212082, rowID)();
+}
+
+SMCBaserow.getManufacturersTable = async function(options){
+    return await getTable(212083, options)();
+}
+
+SMCBaserow.getManufacturer = async function(rowID){
+    return await getRow(212083, rowID)();
+}
+
+SMCBaserow.getClassesTable = async function(options){
+    return await getTable(212084, options)();
+}
+
+SMCBaserow.getClass = async function(rowID){
+    return await getRow(212084, rowID)();
+}
+
+export default SMCBaserow;
