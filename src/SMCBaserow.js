e08a534b56f4063c015be7cf43ba7eabc092dade
Austin Decker <97706910+AustinDecker@users.noreply.github.com>
2023-12-08 08:07:57 -0500

    Removed private api key from file
    You will now have to locally assign the API_KEY with your private token, or load it in via an env file.

--- src/SMCBaserow.js
+++ src/SMCBaserow.js
@@ -1,6 +1,6 @@
 import Baserow from "../src/Baserow.js";
 
-const API_KEY = "Token TzDtpdxtxo0iUq8HKNB2Eisv433d2Auy"; //would be moved to .env later
+const API_KEY = ""; //would be moved to .env later
 
 const TableIDs = 
 {
